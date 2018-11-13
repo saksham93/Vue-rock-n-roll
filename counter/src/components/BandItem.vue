44ddb9dc7338e60e375a0ca145a0a7eddbdf6072
NAGARRO\sakshamgupta <saksham.gupta@nagarro.com>
2018-11-13 12:40:54 +0530

    First commit

---
+++ counter/src/components/BandItem.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <li @click="toggle()">
+            {{band.name}}
+            <p>
+                <span v-show="open">
+                    {{band.songs | join}}
+                    <input type="text" placeholder="Add song" v-model="songName" @keyup.enter="addSong" @click.stop>
+                </span>
+            </p>
+        </li>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'BandItem',
+    data () {
+        return {
+            open: false,
+            songName: ''
+        }
+    },
+    methods: {
+        toggle () {
+                this.open = !this.open
+        },
+        addSong () {
+            this.band.songs.push(this.songName);
+            this.songName = '';
+        }
+    },
+    props: {
+        band: Object
+    },
+   filters: {
+    join (arr) {
+      return arr.join(', ')
+    }
+  }
+}
+</script>
+
---
+++ counter/src/components/BandsList.vue
@@ -0,0 +1,52 @@
+<template>
+    <div>
+        <input type="text" placeholder="Add Banc" v-model="newBand" @keyup.enter="addBand()">
+        <button @click="addBand()">Add</button>
+        <ul>
+            <BandItem v-for="band in bands" :key="band.id" :band="band">
+            </BandItem>
+        </ul>
+    </div>
+</template>
+
+<script>
+import BandItem from './BandItem.vue'
+
+export default {
+
+    name: 'BandsList',
+    data () {
+        return {
+            bands: [
+                {
+                    id: 1,
+                    name: 'Blak',
+                    songs: [
+                        "Iron Man", "War Pigs"
+                    ]
+                },
+                {
+                    id: 2,
+                    name: 'ColdPlay',
+                    songs: ["Yellow", "Fix Yout"]
+                }
+            ],
+            newBand: null
+
+        }
+    },
+    components: {
+        BandItem
+    },
+    methods: {
+        addBand () {
+            this.bands.push({
+                name: this.newBand,
+                id: this.bands.length+1,
+                songs: []
+            });
+            this.newBand = '';
+        }
+    }
+}
+</script>
